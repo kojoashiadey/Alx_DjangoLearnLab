25ce3fa78692456c739380726928cc6a20d27033
kojoashiadey <161717306+kojoashiadey@users.noreply.github.com>
2025-09-19 16:51:45 +0000

    Create views.py

---
+++ api_project/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Require login
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]  # Only admins can create/update/delete
